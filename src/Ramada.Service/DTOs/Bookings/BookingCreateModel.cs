528aeb2eba54ad7fd74bc9009b76a61ee73eea0b
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-04-21 17:12:00 +0500

    Update BookingCreateModel.cs

--- src/Ramada.Service/DTOs/Bookings/BookingCreateModel.cs
+++ src/Ramada.Service/DTOs/Bookings/BookingCreateModel.cs
@@ -5,6 +5,7 @@ namespace Ramada.Service.DTOs.Bookings;
 
 public class BookingCreateModel
 {
+    public long CustomerId { get; set; }
     public long RoomId { get; set; }
     public int NumberOfPeople { get; set; }
     public DateTime StartDate { get; set; }
