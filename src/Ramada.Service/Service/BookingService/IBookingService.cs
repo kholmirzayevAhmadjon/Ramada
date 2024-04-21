21c87ee1c3664cfb71fe8e095f8a57db42b38e2c
Sayidahror <saidahrormirzaahmedov@gmail.com>
2024-04-21 09:29:12 +0500

    Added Partial Service

---
+++ src/Ramada.Service/Service/BookingService/IBookingService.cs
@@ -0,0 +1,18 @@
+﻿using Ramada.Service.Configurations;
+using Ramada.Service.DTOs.Bookings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ramada.Service.Service.BookingService;
+
+public interface IBookingService
+{
+    ValueTask<BookingViewModel> CreateAsync(BookingCreateModel bookingCreateModel);
+    ValueTask<BookingViewModel> GetAsync(long id);
+    ValueTask<IEnumerable<BookingViewModel>> GetAll(PaginationParams @params, Filter filter, string search = null);
+    ValueTask<BookingViewModel> UpdateAsync(long id, BookingUpdateModel bookingUpdateModel);
+    ValueTask<BookingViewModel> DeleteAsync(long id);
+}
