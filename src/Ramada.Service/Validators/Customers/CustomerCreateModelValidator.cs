9947f5ecdcfd964b4fffee39740b297a7bb3698e
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-04-24 15:40:05 +0500

    Added CustomerCreateModelValidator and CustomerUpdateModelValidator

---
+++ src/Ramada.Service/Validators/Customers/CustomerCreateModelValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Ramada.Service.DTOs.Customers;
+using Ramada.Service.Validators.Facilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ramada.Service.Validators.Customers;
+
+public class CustomerCreateModelValidator : AbstractValidator<CustomerCreateModel>
+{
+    public CustomerCreateModelValidator()
+    {
+        RuleFor(customer => customer.FirstName)
+            .NotNull()
+            .WithMessage(customer => $"{nameof(customer.FirstName)} is not specified");
+
+        RuleFor(customer => customer.LastName)
+            .NotNull()
+            .WithMessage(customer => $"{nameof(customer.LastName)} is not specified");
+
+        RuleFor(customer => customer.UserId)
+            .NotNull()
+            .NotEqual(0)
+            .WithMessage(customer => $"{nameof(customer.UserId)} is not specified");
+    }
+}
---
+++ src/Ramada.Service/Validators/Customers/CustomerUpdateModelValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Ramada.Service.DTOs.Customers;
+
+namespace Ramada.Service.Validators.Customers;
+
+public class CustomerUpdateModelValidator : AbstractValidator<CustomerUpdateModel>
+{
+    public CustomerUpdateModelValidator() 
+    {
+        RuleFor(customer => customer.FirstName)
+            .NotNull()
+            .WithMessage(customer => $"{nameof(customer.FirstName)} is not specified");
+
+        RuleFor(customer => customer.LastName)
+            .NotNull()
+            .WithMessage(customer => $"{nameof(customer.LastName)} is not specified");
+
+        RuleFor(customer => customer.UserId)
+            .NotNull()
+            .NotEqual(0)
+            .WithMessage(customer => $"{nameof(customer.UserId)} is not specified");
+    }
+}
