0cad56e3dc292326120a6ec74db0ca052c08e0ad
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-04-24 13:51:59 +0500

    Added PermissionCreateModelValidator and PermissionUpdateModelValidator

---
+++ src/Ramada.Service/Validators/Permissions/PermissionUpdateModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Ramada.Domain.Entities.Users;
+
+namespace Ramada.Service.Validators.Permissions;
+
+public class PermissionUpdateModelValidator : AbstractValidator<PermissionUpdateModel>
+{
+    public PermissionUpdateModelValidator()
+    {
+        RuleFor(permission => permission.Method)
+           .NotNull()
+           .WithMessage(permission => $"{nameof(permission.Method)} is not specified");
+
+        RuleFor(permission => permission.Controller)
+           .NotNull()
+           .WithMessage(permission => $"{nameof(permission.Controller)} is not specified");
+    }
+}
