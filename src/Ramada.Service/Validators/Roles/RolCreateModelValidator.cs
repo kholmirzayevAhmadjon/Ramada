c80a9eb7beaff9e60b1d6819671387bd50fceabc
Ahmadjon <xolmirzayevahmadjon877@gmail.com>
2024-04-24 13:46:55 +0500

    Added RoleCreateModelValidator and RoleUpdateModelValidator

---
+++ src/Ramada.Service/Validators/Roles/RolCreateModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Ramada.Service.DTOs.Roles;
+
+namespace Ramada.Service.Validators.Roles;
+
+public class RolCreateModelValidator : AbstractValidator<RoleCreateModel>
+{
+    public RolCreateModelValidator()
+    {
+        RuleFor(role => role.Name)
+            .NotNull()
+            .WithMessage(role => $"{nameof(role.Name)} is not specified");
+    }
+}
